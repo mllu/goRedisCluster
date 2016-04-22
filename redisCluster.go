ddb85e8b4fd7fa348684003e321fcb7246492ba3
meng lin <menglin@apsalar.com>
2016-04-22 14:10:56 -0700

    use redigo & related cluster lib to set up rediscluster & add test for timeout

--- redisCluster.go
+++ redisCluster.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
 	"strings"
 	"time"
 
-	"./lib"
-	"github.com/mediocregopher/radix.v2/cluster"
-	"github.com/mediocregopher/radix.v2/redis"
+	rd "github.com/chasex/redis-go-cluster"
+	"github.com/garyburd/redigo/redis"
 )
 
 const (
@@ -16,13 +20,22 @@ const (
 )
 
 var (
-	Infof   = lib.Infof
-	Infoln  = lib.Infoln
-	Debugf  = lib.Debugf
-	Debugln = lib.Debugln
-	Errorf  = lib.Errorf
-	Errorln = lib.Errorln
+	Info  *log.Logger
+	Debug *log.Logger
+	Warn  *log.Logger
+	Error *log.Logger
 )
+
+func Init(infoHandle, debugHandle, warnHandle, errorHandle io.Writer) {
+	Info = log.New(infoHandle, "[INFO]  ", log.Ldate|log.Ltime|log.Lshortfile)
+	Debug = log.New(debugHandle, "[DEBU] ", log.Ldate|log.Ltime|log.Lshortfile)
+	Warn = log.New(warnHandle, "[WARN]  ", log.Ldate|log.Ltime|log.Lshortfile)
+	Error = log.New(errorHandle, "[ERRO] ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func init() {
+	Init(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
+}
 
 type Node struct {
 	IP     string
@@ -40,8 +53,28 @@ func (n *Node) String() string {
 	}
 }
 
-func getCluster() *cluster.Cluster {
-	cluster, err := cluster.New(ADMIN)
+func getCluster(connTimeout, readTimeout, writeTimeout time.Duration) *rd.Cluster {
+	cluster, err := rd.NewCluster(
+		&rd.Options{
+			StartNodes: []string{
+				"127.0.0.1:7001",
+				"127.0.0.1:7002",
+				"127.0.0.1:7003",
+				"127.0.0.1:7004",
+				"127.0.0.1:7005",
+				"127.0.0.1:7006",
+			},
+			ConnTimeout:  connTimeout,
+			ReadTimeout:  readTimeout,
+			WriteTimeout: writeTimeout,
+			/*
+				ConnTimeout:  100 * time.Microsecond,
+				ReadTimeout:  3 * time.Microsecond,
+				WriteTimeout: 3 * time.Microsecond,
+			*/
+			KeepAlive: 16,
+			AliveTime: 60 * time.Second,
+		})
 	if err != nil {
 		panic(err)
 	}
@@ -50,20 +83,20 @@ func getCluster() *cluster.Cluster {
 func loadClusterInfo() (masters, slaves map[string]*Node) {
 	// load redis-cluster masters&slaves ip/port info
 	masters = map[string]*Node{
-		"10.0.1.42:7001": &Node{"10.0.1.42", "7001", "", ""},
-		"10.0.1.42:7002": &Node{"10.0.1.42", "7002", "", ""},
-		"10.0.1.42:7003": &Node{"10.0.1.42", "7003", "", ""},
+		"127.0.0.1:7001": &Node{"127.0.0.1", "7001", "", ""},
+		"127.0.0.1:7002": &Node{"127.0.0.1", "7002", "", ""},
+		"127.0.0.1:7003": &Node{"127.0.0.1", "7003", "", ""},
 	}
 	slaves = map[string]*Node{
-		"10.0.1.42:7004": &Node{"10.0.1.42", "7004", "", "10.0.1.42:7001"},
-		"10.0.1.42:7005": &Node{"10.0.1.42", "7005", "", "10.0.1.42:7002"},
-		"10.0.1.42:7006": &Node{"10.0.1.42", "7006", "", "10.0.1.42:7003"},
+		"127.0.0.1:7004": &Node{"127.0.0.1", "7004", "", "127.0.0.1:7001"},
+		"127.0.0.1:7005": &Node{"127.0.0.1", "7005", "", "127.0.0.1:7002"},
+		"127.0.0.1:7006": &Node{"127.0.0.1", "7006", "", "127.0.0.1:7003"},
 	}
 	return
 }
 
 func joinCluster(nodes, masters, slaves map[string]*Node) {
-	Infoln("===========================  Join Cluster ===========================")
+	Info.Println("===========================  Join Cluster ===========================")
 	// set up admin
 	c, err := redis.Dial("tcp", ADMIN)
 	if err != nil {
@@ -72,22 +105,18 @@ func joinCluster(nodes, masters, slaves map[string]*Node) {
 	defer c.Close()
 	// meet all masters and slaves
 	for k, v := range masters {
-		c.Cmd("CLUSTER", "MEET", v.IP, v.Port)
+		c.Do("CLUSTER", "MEET", v.IP, v.Port)
 		nodes[k] = v
 	}
 
 	for k, v := range slaves {
-		c.Cmd("CLUSTER", "MEET", v.IP, v.Port)
+		c.Do("CLUSTER", "MEET", v.IP, v.Port)
 		nodes[k] = v
 	}
 
 	// construct nodeID-address map
-	time.Sleep(time.Duration(1500 * time.Millisecond))
-	resp := c.Cmd("CLUSTER", "NODES")
-	if err != nil {
-		panic(err)
-	}
-	str, err := resp.Str()
+	time.Sleep(time.Duration(1000 * time.Millisecond))
+	str, err := redis.String(c.Do("CLUSTER", "NODES"))
 	if err != nil {
 		panic(err)
 	}
@@ -101,35 +130,33 @@ func joinCluster(nodes, masters, slaves map[string]*Node) {
 		}
 	}
 	for _, v := range nodes {
-		Infoln(v)
+		Info.Println(v)
 	}
 }
 
 // set up master-slave relationship
 func setupMasterSlave(masters, slaves map[string]*Node) {
-	Infoln("=========================== Msater-Slave ===========================")
-	var resp *redis.Resp
+	Info.Println("=========================== Msater-Slave ===========================")
 	for k, v := range slaves {
 		conn, err := redis.Dial("tcp", k)
 		if err != nil {
 			panic(err)
 		}
-		resp = conn.Cmd("CLUSTER", "REPLICATE", masters[v.Master].NodeId)
-		Debugln("CLUSTER REPLICATE RESULT", resp)
+		resp, err := conn.Do("CLUSTER", "REPLICATE", masters[v.Master].NodeId)
+		Debug.Println("CLUSTER REPLICATE RESULT", resp)
 		conn.Close()
 	}
 }
 
 func allocHashSlots(masters map[string]*Node) {
-	Infoln("=========================== Alloc Slots ===========================")
-	var resp *redis.Resp
+	Info.Println("=========================== Alloc Slots ===========================")
 	each := TOTALSLOTS / len(masters)
-	Debugf("Total Slots: %v, number of masters: %v, number of each hash slots: %v", TOTALSLOTS, len(masters), each)
-	var array [TOTALSLOTS]int
+	Debug.Printf("Total Slots: %v, number of masters: %v, number of each hash slots: %v", TOTALSLOTS, len(masters), each)
+	var array [TOTALSLOTS]string
 	for n, _ := range array {
-		array[n] = n
+		array[n] = strconv.Itoa(n)
 	}
-	slice := make([][]int, len(masters))
+	slice := make([][]string, len(masters))
 	var n int = 0
 	for n = 0; n < (len(masters) - 1); n++ {
 		slice[n] = array[n*each : (n+1)*each]
@@ -141,43 +168,69 @@ func allocHashSlots(masters map[string]*Node) {
 		if err != nil {
 			panic(err)
 		}
-		resp = conn.Cmd("CLUSTER", "ADDSLOTS", slice[n])
-		Debugln("CLUSTER ADDSLOTS RESULT", resp)
-		conn.Close()
+		defer conn.Close()
+		for _, v := range slice[n] {
+			err := conn.Send("CLUSTER", "ADDSLOTS", v)
+			if err != nil {
+				panic(err)
+			}
+		}
+		conn.Flush()
+		resp, err := conn.Receive()
+		Debug.Println("CLUSTER ADDSLOTS RESULT", resp)
 		n++
 	}
+	time.Sleep(time.Duration(500 * time.Millisecond))
 }
 
-func verifyRedis() {
-	Infoln("=========================== Redis Verification  ===========================")
-	var resp *redis.Resp
-	cluster := getCluster()
-	defer cluster.Close()
-	resp = cluster.Cmd("CLUSTER", "NODES")
-	str, err := resp.Str()
+func verifyCluster() {
+	Info.Println("=========================== Redis Verification  ===========================")
+	cluster := getCluster(100*time.Microsecond, 100*time.Microsecond, 100*time.Microsecond)
+	str, err := rd.String(cluster.Do("CLUSTER", "NODES"))
 	if err != nil {
 		panic(err)
 	}
 	arr := strings.Split(str, "\n")
 	for _, v := range arr {
 		if v != "" {
-			Infoln(v)
+			Info.Println(v)
 		}
 	}
-	Infoln("=========================== Simple Set-Get  ===========================")
-	Debugln("SET foo bar")
-	resp = cluster.Cmd("SET", "foo", "bar")
-	Debugln("GET foo")
-	resp = cluster.Cmd("GET", "foo")
-	Debugln("===>", resp)
+
+	Info.Println("=========================== Simple Set-Get  ===========================")
+	Debug.Println("SET foo bar")
+	resp, err := cluster.Do("SET", "foo", "bar")
+	if err == nil {
+		Debug.Println(resp, ", then GET foo")
+		str, err = rd.String(cluster.Do("GET", "foo"))
+		Debug.Println("===>", str)
+	}
+	cluster.Close()
 }
 
-func main() {
-	lib.SetLevel(lib.DebugLevel)
+func testTimeout() {
+	Info.Println("=========================== Timeout Testing  ===========================")
+	cluster := getCluster(100*time.Microsecond, 2*time.Microsecond, 2*time.Microsecond)
+	Debug.Println("GET foo")
+	str, err := rd.String(cluster.Do("GET", "foo"))
+	Debug.Println("===>", str, "with", err)
+	cluster.Close()
+}
+
+func setupCluster() {
 	nodes := make(map[string]*Node)
 	masters, slaves := loadClusterInfo()
 	joinCluster(nodes, masters, slaves)
 	setupMasterSlave(masters, slaves)
 	allocHashSlots(masters)
-	verifyRedis()
+}
+func main() {
+	timeout := flag.Bool("t", false, "timeout testing")
+	flag.Parse()
+	if *timeout {
+		testTimeout()
+		return
+	}
+	setupCluster()
+	verifyCluster()
 }
